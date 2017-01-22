abea59ae6e4a6af947efe1d1712247a0cb2a20e1
Geoff Cox <redgeoff@gmail.com>
2017-01-21 20:37:57 -0800

    fix(munin): need to use ip address

--- munin.sh
+++ munin.sh
@@ -10,8 +10,7 @@ echo "192.168.50.18 munin.dev" >> /etc/hosts
 apt-get install -y munin-node
 
 # Point to Munin master. Note: periods need to be escaped as regex
-echo "allow ^munin\.dev$" >> /etc/munin/munin-node.conf
-# echo "allow ^192\.168\.50\.18$" >> /etc/munin/munin-node.conf
+echo "allow ^192\.168\.50\.18$" >> /etc/munin/munin-node.conf
 
 # Restart Munin
 service munin-node restart
